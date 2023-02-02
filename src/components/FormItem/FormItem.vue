ddab564e126db526ca105bf21d4a4ae88d95e907
yamid-granda <yamid.granda@gmail.com>
2023-02-02 05:14:16 -0500

    feat: FormItem component

---
+++ src/components/FormItem/FormItem.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="ss-form-item">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.ss-form-item {
+  margin: $input-gutter 0;
+}
+</style>
