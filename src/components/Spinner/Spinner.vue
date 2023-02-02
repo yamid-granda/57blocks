49103f29eec9b08f771f4f3df9eefb07dc6b9eb0
yamid-granda <yamid.granda@gmail.com>
2023-02-02 05:14:36 -0500

    feat: Spinner component

---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import SpinnerIcon from '../../icons/SpinnerIcon.vue'
+</script>
+
+<template>
+  <SpinnerIcon class="ss-spinner" />
+</template>
+
+<style lang="scss">
+.ss-spinner {
+  animation: $animation-spin;
+}
+</style>
