055ed0fd04ce397561ce4a07455d070f2beca97b
yamid-granda <yamid.granda@gmail.com>
2023-02-01 20:49:45 -0500

    feat: SVG wrap component

---
+++ src/components/SVG/SVG.vue
@@ -0,0 +1,27 @@
+<!-- + Icons from https://feathericons.com  -->
+<!-- https://freeicons.io/illustration-list/business-illustration -->
+
+<script lang="ts" setup>
+</script>
+
+<template>
+  <svg
+    class="ss-icon"
+    viewBox="0 0 24 24"
+    width="1em"
+    height="1em"
+    stroke="currentColor"
+    stroke-width="2"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <slot />
+  </svg>
+</template>
+
+<style lang="scss">
+.ss-icon {
+  transition: $transition-base;
+}
+</style>
