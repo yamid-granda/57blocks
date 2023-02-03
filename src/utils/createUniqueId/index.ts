5e3a1cfee058643845f046e16f255e96462e0ed9
yamid-granda <yamid.granda@gmail.com>
2023-02-03 17:39:26 -0500

    feat: create uniqueId util

---
+++ src/utils/createUniqueId/index.ts
@@ -0,0 +1,14 @@
+export const createUniqueIdFromDate = (date: Date): string => {
+  const timestamp = date.getTime()
+  const base36Timestamp = timestamp.toString(36)
+  const id
+    = base36Timestamp
+    + Math.random().toString(36).slice(-4)
+    + Math.random().toString(36).slice(-4)
+    + Math.random().toString(36).slice(-4)
+  return id
+}
+
+export const createUniqueId = (): string => {
+  return createUniqueIdFromDate(new Date())
+}
