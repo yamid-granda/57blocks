ee7363c30cddba23bcc2de482c0599b2287fe2c8
yamid-granda <yamid.granda@gmail.com>
2023-02-03 17:41:03 -0500

    feat: login page

---
+++ src/pages/login/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import Login from '~/modules/Login/components/Login/Login.vue'
+</script>
+
+<template>
+  <div class="ss-login-page">
+    <Login />
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: guest
+  </route>
