6b173ce7251400ab64572c7fed5fc2dd2ff426ad
yamid-granda <yamid.granda@gmail.com>
2023-02-03 17:28:02 -0500

    feat: loggedUser composable

---
+++ src/composables/loggedUser/index.ts
@@ -0,0 +1,18 @@
+export interface LoggedUser {
+  id: string
+  email: string
+}
+
+const emptyUser: LoggedUser = { id: '', email: '' }
+
+export const loggedUser = ref<LoggedUser>(emptyUser)
+
+export const isUserLoggedIn = computed<boolean>(() => Boolean(loggedUser.value.email))
+
+export function setLoggedUser(user: LoggedUser): void {
+  loggedUser.value = user
+}
+
+export function removeLoggedUser(): void {
+  loggedUser.value = emptyUser
+}
