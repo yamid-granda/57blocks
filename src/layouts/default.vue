b0d1dc41b1d2f489211e82b8452be3e88f0c7a91
yamid-granda <yamid.granda@gmail.com>
2023-02-03 17:37:51 -0500

    chore: default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,19 @@
+<script lang="ts" setup>
+import { isUserLoggedIn } from '~/composables/loggedUser'
+import { ViewPath } from '~/configs/ViewPath'
+
+const router = useRouter()
+
+if (!isUserLoggedIn.value)
+  router.push(ViewPath.LOGIN)
+</script>
+
 <template>
   <main>
     <AppContainer>
-      <RouterView />
+      <template v-if="isUserLoggedIn">
+        <RouterView />
+      </template>
     </AppContainer>
   </main>
 </template>
