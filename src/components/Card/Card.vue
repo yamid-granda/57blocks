1c1445996673c1c7b131ee0730ee4747c533b43f
yamid-granda <yamid.granda@gmail.com>
2023-02-04 17:25:27 -0500

    feat: Card component

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="ss-card">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.ss-card {
+  flex-grow: 1;
+  padding: $input-gutter;
+  border-radius: $border-radius-base;
+  background-color: $color-paper-2;
+  color: $color-text;
+  transition: $transition-base;
+
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  & + & {
+    margin-top: $input-gutter;
+  }
+}
+</style>
