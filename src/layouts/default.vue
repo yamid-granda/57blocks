87c46d7996ce20b6f58dde5a8a43f5471b26899a
yamid-granda <yamid.granda@gmail.com>
2023-02-04 17:28:08 -0500

    feat: add navigation to default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,19 +1,36 @@
 <script lang="ts" setup>
-import { isUserLoggedIn } from '~/composables/loggedUser'
+import { isUserLoggedIn, setLoggedUser } from '~/composables/loggedUser'
 import { ViewPath } from '~/configs/ViewPath'
+import { getLoggedUserFromLocalStorage } from '~/services/localStorage/loggedUser'
 
+const { t } = useI18n()
 const router = useRouter()
+
+const response = getLoggedUserFromLocalStorage()
+
+if (response.isOk)
+  setLoggedUser(response.result)
 
 if (!isUserLoggedIn.value)
   router.push(ViewPath.LOGIN)
 </script>
 
 <template>
-  <main>
-    <AppContainer>
-      <template v-if="isUserLoggedIn">
+  <div
+
+    class="ss-default-template"
+  >
+    <main v-if="isUserLoggedIn">
+      <AppContainer>
         <RouterView />
-      </template>
-    </AppContainer>
-  </main>
+      </AppContainer>
+    </main>
+
+    <Navigation
+      :links="[
+        { path: ViewPath.HOME, text: t('Home'), icon: 'home' },
+        { path: ViewPath.FAVORITE_POKEMONS, text: t('Favorites'), icon: 'heart' },
+      ]"
+    />
+  </div>
 </template>
