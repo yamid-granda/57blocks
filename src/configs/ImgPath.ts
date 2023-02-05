291e538660e9dfda007a91218f555fc1909c63e7
yamid-granda <yamid.granda@gmail.com>
2023-02-05 09:18:46 -0500

    chore: ImgPath config

---
+++ src/configs/ImgPath.ts
@@ -0,0 +1,4 @@
+export enum ImgPath {
+  POKEMON_IMAGE = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/{pokemonId}',
+  POKEMON_IMAGE_NOT_FOUND = 'https://www.svgrepo.com/show/276264/pokeball-pokemon.svg',
+}
