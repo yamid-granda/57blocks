d82a34db42d1f458b2f8e0db9ffdcdb568bb437e
yamid-granda <yamid.granda@gmail.com>
2023-02-05 11:50:11 -0500

    feat: favorite pokemons page

---
+++ src/pages/pokemon/favorites/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import FavoritePokemonsViewer from '~/modules/Pokemon/components/FavoritePokemonsViewer/FavoritePokemonsViewer.vue'
+</script>
+
+<template>
+  <div class="ss-favorite-pokemons-page">
+    <FavoritePokemonsViewer />
+  </div>
+</template>
