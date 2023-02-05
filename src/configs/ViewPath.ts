e1a4a2c942352b123f4677a455bbf4503f87a3c9
yamid-granda <yamid.granda@gmail.com>
2023-02-05 11:48:03 -0500

    fix: pokemon detail path

--- src/configs/ViewPath.ts
+++ src/configs/ViewPath.ts
@@ -2,5 +2,5 @@ export enum ViewPath {
   HOME = '/',
   LOGIN = '/login',
   FAVORITE_POKEMONS = '/pokemon/favorites',
-  POKEMON_DETAIL = 'pokemon/{pokemonId}',
+  POKEMON_DETAIL = '/pokemon/{pokemonId}',
 }
