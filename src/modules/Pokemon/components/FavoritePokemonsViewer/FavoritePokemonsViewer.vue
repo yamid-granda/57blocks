3683daf9759c0151043b6505fbe221ed287abbe9
yamid-granda <yamid.granda@gmail.com>
2023-02-05 11:47:32 -0500

    feat: FavoritePokemonsViewer component

---
+++ src/modules/Pokemon/components/FavoritePokemonsViewer/FavoritePokemonsViewer.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { getFavoritePokemonsByUserIdFromStorage } from '../../services/localStorage/pokemon'
+import type { FavoritePokemonsDic, Pokemon } from '../../types'
+import PokemonList from '../PokemonList/PokemonList.vue'
+import { loggedUser } from '~/composables/loggedUser'
+
+const { t } = useI18n()
+
+const favoritePokemonsByUserId = ref<Record<string, FavoritePokemonsDic>>({})
+
+const pokemons = computed<Pokemon[]>(() => {
+  const pokemonsDic = favoritePokemonsByUserId.value[loggedUser.value.id]
+  return Object.values(pokemonsDic).map(pokemon => ({ ...pokemon, isFavorite: true }))
+})
+
+const hasFavorites = computed(() => Boolean(pokemons.value.length))
+
+function onRemoveFavorite(): void {
+  getFavoritesByUserId()
+}
+
+function onCreate(): void {
+  getFavoritesByUserId()
+}
+
+onCreate()
+
+function getFavoritesByUserId(): void {
+  const response = getFavoritePokemonsByUserIdFromStorage()
+
+  if (!response.isOk)
+    return
+
+  favoritePokemonsByUserId.value = response.result
+}
+</script>
+
+<template>
+  <div
+    class="ss-favorite-pokemons"
+    test-id="favorite-pokemons"
+  >
+    <template v-if="hasFavorites">
+      <PokemonList
+        :pokemons="pokemons"
+        @click-favorite="onRemoveFavorite"
+      />
+    </template>
+
+    <template v-else>
+      <Alert state="warning">
+        {{ t('$components.FavoritePokemonsViewer.notFoundError') }}
+      </Alert>
+    </template>
+  </div>
+</template>
