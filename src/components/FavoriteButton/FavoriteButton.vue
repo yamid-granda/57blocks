2b2e539c82f6a075bf5c01a2816598399aa86724
yamid-granda <yamid.granda@gmail.com>
2023-02-05 09:17:25 -0500

    feat: FavoriteButton component

---
+++ src/components/FavoriteButton/FavoriteButton.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+interface FavoriteButtonProps {
+  isActive?: boolean
+}
+
+const props = defineProps<FavoriteButtonProps>()
+</script>
+
+<template>
+  <Button
+    class="ss-favorite-button"
+    :class="{
+      'ss-favorite-button--active': props.isActive,
+    }"
+    icon="heart"
+    design="secondary"
+    size="sm"
+    test-id="favorite"
+  />
+</template>
+
+<style lang="scss">
+.ss-favorite-button--active {
+  path {
+    fill: $color-danger;
+    stroke: $color-danger;
+  }
+}
+</style>
